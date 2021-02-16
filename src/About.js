cc1f9b7e8ce9652b761b5b9927de6ced25a6b3fe
Ben <ben10baek@hotmail.com>
2021-02-15 20:41:18 -0800

    Added images to about page

--- src/About.js
+++ src/About.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import hangingShoes from './img/hangingShoes.jpg';
+import streetProfile from './img/streetpic.jpg';
 
 const About = () => {
 	return (
 		<div>
 			<h1>About</h1>
 			<div>
-				<p>Welcome to the shop</p>
+				<p>Welcome to Fresh Kicks</p>
+				<p>
+					Started in during the Covid-19 pandemic of 2020. We wanted to allow an affordable place of
+					people to enjoy shoes at home.
+				</p>
+			</div>
+			<div className="row">
+				<div className="col-sm-6">
+					<img src={hangingShoes} className="img-responsive" alt="Shoes hanging over ledge"></img>
+				</div>
+				<div className="col-sm-4">
+					<img
+						src={streetProfile}
+						className="img-responsive"
+						alt="Street profile by building"></img>
+				</div>
 			</div>
 		</div>
 	);
--- src/Cart.js
+++ src/Cart.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Cart.module.css';
 
 const Cart = (props) => {
 	const { totalItems, displayCart, clearCart } = props;
 	return (
-		<div>
+		<div className={styles.fill}>
 			<div>
 				Number of Items: {totalItems}
 				<ul className="cart">{displayCart}</ul>
